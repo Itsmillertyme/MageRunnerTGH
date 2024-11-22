cab0cd19e72caa836273cf87e17d3f50a5d14b09
Jacob M <31823746+Itsmillertyme@users.noreply.github.com>
2024-11-22 14:34:03 -0500

    Final prototype work
    - Added overlays that fade in and out at beginning and end of demo level
    - Added font asset for use in overlays

--- TGH_MageGame/Assets/Scripts/GameManager.cs
+++ TGH_MageGame/Assets/Scripts/GameManager.cs
@@ -1,13 +1,28 @@
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
     [SerializeField] Canvas hud;
     [SerializeField] Transform projectileSpawn;
     [SerializeField] Transform player;
+    [SerializeField] GameObject introOverlayPanel;
+    [SerializeField] GameObject outroOverlayPanel;
+    [SerializeField] AudioClip fireFX;
+    [SerializeField] AudioClip levelSound;
+
+    public bool playOutro;
+    bool outroPlayed = false;
+
     Vector3 playerPivot;
+    Vector3 introOverlayPanelHiddenPos = new Vector3(0, -500, 0);
+    Vector3 outroOverlayPanelHiddenPos = new Vector3(0, -1500, 0);
+
 
     //DEV ONLY - REMOVE BEFORE BUILD
     Transform mousePositionObject;
@@ -24,10 +39,37 @@ public class GameManager : MonoBehaviour {
                 Debug.LogWarning(bug);
             }
         }
+
+        //DEMO OVERLAY CODE
+        //set sound
+        Camera cam = Camera.main;
+        cam.GetComponent<AudioSource>().resource = fireFX;
+        cam.GetComponent<AudioSource>().Play();
+
+        //get tmp assets
+        TextMeshProUGUI[] introTMPs = introOverlayPanel.GetComponentsInChildren<TextMeshProUGUI>();
+
+        //set intro panel pos
+        introOverlayPanel.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, 0, 0);
+
+        //hide text
+        for (int i = 0; i < introTMPs.Length; i++) {
+            introTMPs[i].color = new Color(1, 1, 1, 0);
+        }
+
+        StartCoroutine(PlayIntroOverlay(introTMPs));
+
     }
 
     private void Update() {
         MoveProjectileSpawn();
+
+        if (!outroPlayed) {
+            if (playOutro) {
+                outroPlayed = true;
+                StartCoroutine(PlayOutroOverlay());
+            }
+        }
     }
 
     private void OnApplicationFocus(bool focus) {
@@ -74,5 +116,91 @@ public class GameManager : MonoBehaviour {
         Application.Quit();
     }
 
+    public void LoadMainMenu() {
+        SceneManager.LoadScene("Splash");
+    }
+
+    IEnumerator PlayIntroOverlay(TextMeshProUGUI[] TMPs) {
+        //stop player input
+        PlayerController pc = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        pc.FreezePhysics = true;
+
+
+        //First text
+        float elapsedTime = 0f;
+        float duration1 = 8;
+        Color originalColor = TMPs[0].color;
+
+
+        while (elapsedTime < duration1) {
+            elapsedTime += Time.deltaTime;
+            float alpha = Mathf.Clamp01(elapsedTime / duration1);
+            TMPs[0].color = new Color(originalColor.r, originalColor.g, originalColor.b, alpha);
+            yield return null;
+        }
+
+        TMPs[0].color = new Color(originalColor.r, originalColor.g, originalColor.b, 1f);
+
+        //Second text
+        elapsedTime = 0f;
+        float duration2 = 12;
+        originalColor = TMPs[1].color;
+        while (elapsedTime < duration2) {
+            elapsedTime += Time.deltaTime;
+            float alpha = Mathf.Clamp01(elapsedTime / duration2);
+            TMPs[1].color = new Color(originalColor.r, originalColor.g, originalColor.b, alpha);
+            yield return null;
+        }
+
+        TMPs[1].color = new Color(originalColor.r, originalColor.g, originalColor.b, 1f);
+
+        Camera cam = Camera.main;
+        cam.GetComponent<AudioSource>().resource = levelSound;
+        cam.GetComponent<AudioSource>().Play();
+
+        pc.FreezePhysics = false;
+        introOverlayPanel.GetComponent<RectTransform>().anchoredPosition = introOverlayPanelHiddenPos;
+    }
+
+    IEnumerator PlayOutroOverlay() {
+        //stop player input
+        PlayerController pc = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        pc.FreezePhysics = true;
+
+        //First text
+        float elapsedTime = 0f;
+        float duration = 1;
+        Image panelImage = outroOverlayPanel.GetComponent<Image>();
+        TextMeshProUGUI outroTMP = outroOverlayPanel.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        Image logoImage = outroOverlayPanel.transform.GetChild(1).GetComponent<Image>();
+        Image mainMenuButtonImage = outroOverlayPanel.transform.GetChild(2).GetComponent<Image>();
+        TextMeshProUGUI buttonTMP = mainMenuButtonImage.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+
+        Color panelOriginalColor = panelImage.color;
+        Color textOriginalColor = outroTMP.color;
+        Color logoOriginalColor = logoImage.color;
+        Color buttonOriginalColor = mainMenuButtonImage.color;
+        Color buttonTMPOriginalColor = buttonTMP.color;
+
+        panelImage.color = new Color(panelOriginalColor.r, panelOriginalColor.g, panelOriginalColor.b, 0);
+        outroTMP.color = new Color(textOriginalColor.r, textOriginalColor.g, textOriginalColor.b, 0);
+        logoImage.color = new Color(logoOriginalColor.r, logoOriginalColor.g, logoOriginalColor.b, 0);
+        mainMenuButtonImage.color = new Color(logoOriginalColor.r, logoOriginalColor.g, logoOriginalColor.b, 0);
+        buttonTMP.color = new Color(logoOriginalColor.r, logoOriginalColor.g, logoOriginalColor.b, 0);
+
+        //move panel intoposition
+        outroOverlayPanel.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, 0, 0);
+
+        while (elapsedTime < duration) {
+            elapsedTime += Time.deltaTime;
+            float alpha = Mathf.Clamp01(elapsedTime / duration);
+            panelImage.color = new Color(panelOriginalColor.r, panelOriginalColor.g, panelOriginalColor.b, alpha);
+            outroTMP.color = new Color(textOriginalColor.r, textOriginalColor.g, textOriginalColor.b, alpha);
+            logoImage.color = new Color(logoOriginalColor.r, logoOriginalColor.g, logoOriginalColor.b, alpha);
+            mainMenuButtonImage.color = new Color(buttonOriginalColor.r, buttonOriginalColor.g, buttonOriginalColor.b, alpha);
+            buttonTMP.color = new Color(buttonTMPOriginalColor.r, buttonTMPOriginalColor.g, buttonTMPOriginalColor.b, alpha);
+            yield return null;
+        }
+    }
 }
 

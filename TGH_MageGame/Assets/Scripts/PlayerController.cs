85e2daa79247b9cc6b741552023a3d73ca7b0fcc
Jacob M <31823746+Itsmillertyme@users.noreply.github.com>
2025-01-24 10:41:44 -0500

    Fixing conversion bugs

--- TGH_MageGame/Assets/Scripts/PlayerController.cs
+++ TGH_MageGame/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using UnityEngine;
-//using UnityEngine.InputSystem;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
